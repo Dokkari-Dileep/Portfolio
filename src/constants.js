acd8d91c1315280ac519337f33ca781da4cfad0d
dokkaridileep <dokkaridileep1@gmail.com>
2025-10-24 23:20:27 +0530

    Updated About section with my details

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,11 @@
 // Skills Section Logo's
 import htmlLogo from "./assets/tech_logo/html.png";
 import cssLogo from "./assets/tech_logo/css.png";
-// import sassLogo from "./assets/tech_logo/sass.png";
+
 import javascriptLogo from "./assets/tech_logo/javascript.png";
 import reactjsLogo from "./assets/tech_logo/reactjs.png";
 // import angularLogo from "./assets/tech_logo/angular.png";
-// import reduxLogo from "./assets/tech_logo/redux.png";
 // import nextjsLogo from "./assets/tech_logo/nextjs.png";
-// import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
-// import gsapLogo from "./assets/tech_logo/gsap.png";
-// import materialuiLogo from "./assets/tech_logo/materialui.png";
 import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
 // import springbootLogo from "./assets/tech_logo/springboot.png";
 import nodejsLogo from "./assets/tech_logo/nodejs.png";
@@ -26,39 +22,15 @@ import gitLogo from "./assets/tech_logo/git.png";
 import githubLogo from "./assets/tech_logo/github.png";
 import vscodeLogo from "./assets/tech_logo/vscode.png";
 // import postmanLogo from "./assets/tech_logo/postman.png";
-// import mcLogo from "./assets/tech_logo/mc.png";
 import figmaLogo from "./assets/tech_logo/figma.png";
 import netlifyLogo from "./assets/tech_logo/netlify.png";
-// import vercelLogo from "./assets/tech_logo/vercel.png";
 import canavasLogo from "./assets/tech_logo/canava.png";
-// import postgreLogo from "./assets/tech_logo/postgre.png";
-// import csharpLogo from "./assets/tech_logo/csharp.png";
-
-// Experience Section Logo's
-// import webverseLogo from "./assets/company_logo/webverse_logo.png";
-// import agcLogo from "./assets/company_logo/agc_logo.png";
-// import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
-
-// Education Section Logo's
-// import glaLogo from "./assets/education_logo/gla_logo.png";
-// import bsaLogo from "./assets/education_logo/bsa_logo.png";
-// import vpsLogo from "./assets/education_logo/vps_logo.png";
 
 // Project Section Logo's
-// import githubdetLogo from "./assets/work_logo/github_det.png";
+
 import gymLogo from "./assets/work_logo/gym.png";
 import travelLogo from "./assets/work_logo/travel.png";
 import lotteryLogo from "./assets/work_logo/lottery.png";
-
-// import csprepLogo from "./assets/work_logo/cs_prep.png";
-// import movierecLogo from "./assets/work_logo/movie_rec.png";
-// import taskremLogo from "./assets/work_logo/task_rem.png";
-// import npmLogo from "./assets/work_logo/npm.png";
-// import webverLogo from "./assets/work_logo/web_dig.png";
-// import cmLogo from "./assets/work_logo/cm.png";
-// import imagesearchLogo from "./assets/work_logo/image_search.png";
-// import removebgLogo from "./assets/work_logo/remove_bg.png";
-// import gymLogo from "./assets/work_logo/gym.png";
 
 export const SkillsInfo = [
   {
@@ -66,28 +38,18 @@ export const SkillsInfo = [
     skills: [
       { name: "HTML", logo: htmlLogo },
       { name: "CSS", logo: cssLogo },
-      // { name: "SASS", logo: sassLogo },
       { name: "JavaScript", logo: javascriptLogo },
       { name: "React JS", logo: reactjsLogo },
-      // { name: "Angular", logo: angularLogo },
-      // { name: "Redux", logo: reduxLogo },
-      //{ name: "Next JS", logo: nextjsLogo },
-      // { name: "Tailwind CSS", logo: tailwindcssLogo },
-      // { name: "GSAP", logo: gsapLogo },
-      // { name: "Material UI", logo: materialuiLogo },
       { name: "Bootstrap", logo: bootstrapLogo },
     ],
   },
   {
     title: "Backend",
     skills: [
-      // { name: "Springboot", logo: springbootLogo },
       { name: "Node JS", logo: nodejsLogo },
       { name: "Express JS", logo: expressjsLogo },
       { name: "MySQL", logo: mysqlLogo },
       { name: "MongoDB", logo: mongodbLogo },
-      // { name: "Firebase", logo: firebaseLogo },
-      // { name: "PostgreSQL", logo: postgreLogo },
     ],
   },
   {
@@ -96,10 +58,7 @@ export const SkillsInfo = [
       { name: "C", logo: cLogo },
       { name: "C++", logo: cppLogo },
       { name: "Java", logo: javaLogo },
-      // { name: "Python", logo: pythonLogo },
-      // { name: "C-Sharp", logo: csharpLogo },
       { name: "JavaScript", logo: javascriptLogo },
-      // { name: "TypeScript", logo: typescriptLogo },
     ],
   },
   {
@@ -108,9 +67,6 @@ export const SkillsInfo = [
       { name: "Git", logo: gitLogo },
       { name: "GitHub", logo: githubLogo },
       { name: "VS Code", logo: vscodeLogo },
-      // { name: "Postman", logo: postmanLogo },
-      // { name: "Compass", logo: mcLogo },
-      // { name: "Vercel", logo: vercelLogo },
       { name: "Netlify", logo: netlifyLogo },
       { name: "Figma", logo: figmaLogo },
       { name: "canvas", logo: canavasLogo },
@@ -126,22 +82,9 @@ export const experiences = [
     company: "Elewayte",
     date: "Role - Intern",
     desc: "I learned the fundamentals of web development by exploring HTML, CSS, and JavaScript, and applied them to create a responsive travel website. The project focused on designing an engaging interface and smooth functionality, allowing users to book trip plans easily while showcasing my ability to build user-friendly, interactive web applications.",
-    // src: "https://www.linkedin.com/in/dokkari-dileep-308806333?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "MySql",
-      // "React JS",
-      // "TypeScript",
-      // "Node JS",
-      // "Tailwind CSS",
-      // "MongoDb",
-      // "Redux",
-      // " Next Js",
-    ],
+
+    skills: ["HTML", "CSS", "JavaScript", "MySql"],
     certificate:
-      // "https://drive.google.com/file/d/1EuS6CdwgyOh1rdeVkuaN93u9nb0M3Z2z/view?usp=sharing",
       "https://drive.google.com/file/d/1pnpZVzogvlR7BWAD4v27CQ3Rmhb2xCOg/view?usp=drivesdk",
   },
   {
@@ -151,69 +94,36 @@ export const experiences = [
     company: "Edunet",
     date: "Role - Intern",
     desc: "I gained practical experience in full-stack development using the MERN stack (MongoDB, Express.js, React, Node.js) by building dynamic, responsive web applications. One such project was a gym management system that allowed users to explore workout plans, manage memberships, and track progress, showcasing my ability to create secure, user-friendly solutions.”.",
-    skills: [
-      "ReactJS",
-      "Express",
-      // "Redux",
-      "JavaScript",
-      // "Tailwind CSS",
-      "HTML",
-      "CSS",
-      // "SQL",
-    ],
+    skills: ["ReactJS", "Express", "JavaScript", "HTML", "CSS"],
     certificate:
       "https://drive.google.com/file/d/14ENjHR5508ap4YTAAeC-XNBVmbnsW3eV/view?usp=sharing",
   },
   {
     id: 2,
-    // img: newtonschoolLogo,
     role: "Oracle-Certificate",
-    // company: "Newton School",
-    // date: "September 2021 - August 2022",
     desc: "I am proud to have achieved the Oracle AI Vector Search Certified Professional certification. This accomplishment enhances my knowledge in Artificial Intelligence and Database Systems, adding strong value to my academic and professional journey as a CSE student. It reflects my dedication to building advanced technical skills and staying aligned with industry trends, while also preparing me for future opportunities in AI-driven applications and intelligent database management.",
-    // skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
     certificate:
       "https://drive.google.com/file/d/1jB5YhWoLaPoEeia33BkCS0qNboHAB64-/view?usp=sharing",
   },
   {
     id: 3,
-    // img: newtonschoolLogo,
     role: "ISC2-Certificate",
-    // company: "Newton School",
-    // date: "September 2021 - August 2022",
     desc: "Achieved the ISC2 Certified in Cybersecurity (CC) certification, demonstrating proficiency in fundamental cybersecurity concepts including security principles, network security, access controls, incident response, and governance. This certification from (ISC)², the world’s leading cybersecurity professional body, highlights my readiness to pursue advanced roles in cybersecurity.",
-    // skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
     certificate:
-      // "https://drive.google.com/file/d/12gkZO1_BhyUgfMSjEDVnkc3ZnIqO7Lsi/view?usp=sharing",
       "https://drive.google.com/file/d/187N420na78-9PuRsUa3blnCoQxFhnfym/view?usp=drivesdk",
   },
   {
     id: 3,
-    // img: newtonschoolLogo,
     role: "MongoDB-Certificate",
-    // company: "Newton School",
-    // date: "September 2021 - August 2022",
     desc: "Successfully completed the MongoDB Basics for Students course and earned certification from MongoDB University. This course provided hands-on experience with key MongoDB concepts, including CRUD operations, data modeling, and aggregation. It strengthened my practical understanding of NoSQL databases and enhanced my ability to design and manage data efficiently. I look forward to applying these skills in real-world projects, contributing to scalable and modern database-driven applications.",
-    // skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
     certificate:
-      // "https://drive.google.com/file/d/1Uw7THtR2mdSbEzvg7q0buhoB2X8Jc8xY/view?usp=sharing",
       "https://drive.google.com/file/d/1Df14VFvtGG98HVAFTwPfd7MOD6AG6iFw/view?usp=drivesdk",
   },
 ];
 
 export const education = [
-  // {
-  //   id: 0,
-  //   // img: glaLogo,
-  //   school: "GLA University, Mathura",
-  //   date: "Sept 2022 - July 2024",
-  //   grade: "7.81 CGPA",
-  //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-  //   degree: "Master of Computer Applications - MCA",
-  // },
   {
     id: 1,
-    // img: bsaLogo,
     school: "Teegala Krishna Reddy Engineering College, Hyderabad",
     date: "Nov 2022 - may 2026",
     grade: "8.5%",
@@ -222,7 +132,6 @@ export const education = [
   },
   {
     id: 2,
-    // img: vpsLogo,
     school: "Sri Gurukula Vidyalayam Junior College For Science, Srikakulam",
     date: "Jan 2020 - April 2022",
     grade: "86.4%",
@@ -231,7 +140,6 @@ export const education = [
   },
   {
     id: 3,
-    // img: vpsLogo,
     school: "Government of High School Palasa, Srikakulam",
     date: "Jun 2015 - May 2020",
     grade: "97%",
@@ -257,18 +165,7 @@ export const projects = [
     description:
       "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
     image: travelLogo,
-    tags: [
-      // "React JS",
-      // "Node.js",
-      // "MongoDB",
-      // "Express",
-      // "HTML",
-      // "CSS",
-      // "JavaScript",
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
+    tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/Dokkari-Dileep/TRAVEL-webpage",
     webapp: "https://exploren-to-travel.netlify.app/",
   },
@@ -282,64 +179,4 @@ export const projects = [
     github: "https://github.com/codingmastr/Movie-Recommendation-App",
     webapp: "https://jockpot-hub.netlify.app/",
   },
-  // {
-  //   id: 3,
-  //   title: "Email Validator NPM Package",
-  //   description:
-  //     "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
-  //   image: npmLogo,
-  //   tags: ["React JS", "Node.js", "NPM", "Validation"],
-  //   github: "https://github.com/codingmastr/cmtk-email-validator",
-  //   webapp: "https://www.npmjs.com/package/cmtk-email-validator",
-  // },
-  //   {
-  //     id: 4,
-  //     title: "Task Reminder Chrome Extension Tool",
-  //     description:
-  //       "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
-  //     image: taskremLogo,
-  //     tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-  //     github: "https://github.com/codingmastr/Task-Reminder-Tool",
-  //     webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Webverse Digital",
-  //     description:
-  //       "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-  //     image: webverLogo,
-  //     tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-  //     github: "https://github.com/codingmastr/Webverse-Digital",
-  //     webapp: "https://webversedigital.com/",
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Coding Master",
-  //     description:
-  //       "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-  //     image: cmLogo,
-  //     tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-  //     github: "https://codingmasterweb.in/",
-  //     webapp: "https://codingmasterweb.in/",
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "Image Search App",
-  //     description:
-  //       "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-  //     image: imagesearchLogo,
-  //     tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-  //     github: "https://github.com/codingmastr/Image-Search-App",
-  //     webapp: "https://imagsearch.netlify.app/",
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Image Background Remover",
-  //     description:
-  //       "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-  //     image: removebgLogo,
-  //     tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-  //     github: "https://github.com/codingmastr/Image-Background-Remover",
-  //     webapp: "https://removeyourbg.netlify.app/",
-  //   },
 ];
